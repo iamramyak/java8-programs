c74dbc494777f3e5c2c68e3845d107dbb62318d4
iamramyak <iamramyak7@gmail.com>
2024-04-19 12:47:19 +0530

    Java8 programs

---
+++ src/main/java/com/example/java8/programs/Java8ProgramsApplication.java
@@ -0,0 +1,13 @@
+package com.example.java8.programs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8ProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8ProgramsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java8/programs/Java8ProgramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java8.programs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8ProgramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
